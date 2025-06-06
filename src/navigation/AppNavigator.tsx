4f7849677ab09f1600acca916c34b847e326b6a6
KevinNobre <kevinchristian.nobre@gmail.com>
2025-06-06 13:16:11 -0300

    feat: AppNavigator para navegação

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen/LoginScreen';
+import RegistroScreen from '../screens/RegistroScreen/RegistroScreen';
+import HomeScreen from '../screens/HomeScreen/HomeScreen';
+import ReporteEmergenciaScreen from '../screens/ReporteEmergencia/ReporteEmergenciaScreen';
+import SelecionarLocalizacaoScreen from '../screens/SelecionarLocalizacaoScreen/SelecionarLocalizacaoScreen';
+
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+  <Stack.Screen name="Login" component={LoginScreen} />
+  <Stack.Screen name="Registro" component={RegistroScreen} />
+  <Stack.Screen name="Home" component={HomeScreen} />
+  <Stack.Screen name="ReporteEmergencia" component={ReporteEmergenciaScreen} />
+  <Stack.Screen name="SelecionarLocalizacao" component={SelecionarLocalizacaoScreen}/>
+
+  </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
